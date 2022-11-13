122e5bb9e4c741bde6fcdd5bb669d995050b3ce0
Ayush <ayushtyagi772@gmail.com>
2022-11-13 11:44:45 +0530

    Ayush got the access

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useReducer, useEffect,} from "react";
+import React, { useState, useReducer, useEffect } from "react";
 import Product from "./components/Product";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
@@ -6,36 +6,35 @@ import reducer from "./reducers/reducer";
 import Nav from "./components/Nav";
 
 const App = () => {
-  const [navDark, setNavDark] = useState('navbar navbar-expand-lg bg-light')
+  const [navDark, setNavDark] = useState("navbar navbar-expand-lg bg-light");
   const [productStyle, setProductStyle] = useState({
-    color: 'black',
-    backgroundColor: '#f5f1f1',
-    boxShadow: '0px 0px 15px black'
-  })
-  const [theme, setTheme] = useState('light-mode');
-  const [text, setText] = useState('Enable dark mode')
-  const handleMode = () =>{
-    if(theme === 'light-mode'){
-      setTheme('dark-mode');
-      setText('Disable dark mode');
+    color: "black",
+    backgroundColor: "#f5f1f1",
+    boxShadow: "0px 0px 15px black",
+  });
+  const [theme, setTheme] = useState("light-mode");
+  const [text, setText] = useState("Enable dark mode");
+  const handleMode = () => {
+    if (theme === "light-mode") {
+      setTheme("dark-mode");
+      setText("Disable dark mode");
       setProductStyle({
-        color: 'white',
-        backgroundColor: '#141d26',
-        boxShadow: '0px 0px 15px white'
-      })
-      setNavDark('navbar navbar-expand-lg bg-dark navbar-dark')
-    }
-    else{
-      setTheme('light-mode');
-      setText('Enable dark mode')
+        color: "white",
+        backgroundColor: "#141d26",
+        boxShadow: "0px 0px 15px white",
+      });
+      setNavDark("navbar navbar-expand-lg bg-dark navbar-dark");
+    } else {
+      setTheme("light-mode");
+      setText("Enable dark mode");
       setProductStyle({
-        color: 'black',
-        backgroundColor: '#f5f1f1',
-        boxShadow: '0px 0px 15px black'
-      })
-      setNavDark('navbar navbar-expand-lg bg-light')
+        color: "black",
+        backgroundColor: "#f5f1f1",
+        boxShadow: "0px 0px 15px black",
+      });
+      setNavDark("navbar navbar-expand-lg bg-light");
     }
-  }
+  };
 
   const initialState = {
     loading: false,
@@ -67,46 +66,49 @@ const App = () => {
     return (
       <div className="loading">
         <div class="spinner-grow text-primary" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-secondary" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-success" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-danger" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-warning" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-info" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-light" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div class="spinner-grow text-dark" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-secondary" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-success" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-danger" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-warning" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-info" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-light" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+        <div class="spinner-grow text-dark" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
       </div>
     );
   }
 
-  
-
-
   return (
     <React.Fragment>
       <div className={theme}>
-      <Navbar state={state} mode={handleMode} text={text} />
-      {/* <Nav navDark={navDark}/> */}
-      <main>
-        <Product state={state} dispatch={dispatch} theme={theme} productStyle={productStyle}/>
-        <Cart state={state} dispatch={dispatch} />
-      </main>
+        <Navbar state={state} mode={handleMode} text={text} />
+        {/* <Nav navDark={navDark}/> */}
+        <main>
+          <Product
+            state={state}
+            dispatch={dispatch}
+            theme={theme}
+            productStyle={productStyle}
+          />
+          <Cart state={state} dispatch={dispatch} />
+        </main>
       </div>
+      <h1>Ayush Daddy</h1>
     </React.Fragment>
   );
 };
